fd78cf8fa75256a2b0b471696e7ace75ba387185
Steven Harms <steven.harms@gsa.gov>
2019-01-14 10:21:18 -0700

    This string has been updated in the current Nessus version

--- jobs/nessus-manager/templates/bin/health.sh
+++ jobs/nessus-manager/templates/bin/health.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+NESSUSD_MESSAGES=/var/vcap/store/nessus-manager/opt/nessus/var/nessus/logs/nessusd.messages
 #check nessus license
-NESSUS_LICENSE=`grep -A1 'nessusd-reloader: started' /var/vcap/store/nessus-manager/opt/nessus/var/nessus/logs/nessusd.messages | tail -n -1 | grep 'Could not validate the license used on this scanner' | wc -l`
+NESSUS_LICENSE=`grep -A1 'nessusd-reloader: started' ${NESSUSD_MESSAGES} | tail -n -1 | grep 'Could not validate the license used on this scanner' | wc -l`
 
 #check plugin age
-PLUGIN_AGE=$(grep 'nessusd-reloader: Reloading nessusd because Plugin auto-update' /var/vcap/store/nessus-manager/opt/nessus/var/nessus/logs/nessusd.messages | tail -n 1| awk -F '[\[\]]' '{print $2}' | { read datefmt ; echo $(( ($(date +%s) - $(date -d "${datefmt}" +%s)) / 86400 )) ; })
+PLUGIN_AGE=$(grep 'Nessus is reloading: Plugin auto-update' ${NESSUSD_MESSAGES} | tail -n 1| awk -F '[\[\]]' '{print $2}' | { read datefmt ; echo $(( ($(date +%s) - $(date -d "${datefmt}" +%s)) / 86400 )) ; })
 
 #emit metrics
 tempfile=`mktemp`
